1009c4ea63b90c33ba5a6f6d482fd127fb5eacd0
mimvoid <mimvoid@proton.me>
2024-12-16 00:57:55 -0500

    rework keymaps

--- lua/zinnia/plugins/telescope.lua
+++ lua/zinnia/plugins/telescope.lua
@@ -58,28 +58,30 @@ return {
     -- pcall(require('telescope').load_extension, 'fzf')
     -- pcall(require('telescope').load_extension, 'ui-select')
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-    vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-    vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
-    vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+    local m = vim.keymap.set
+    local tl = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>f/", function()
-      builtin.live_grep({
+    m("n", "<leader>fh", tl.help_tags, { desc = "[F]ind [H]elp" })
+    m("n", "<leader>fk", tl.keymaps, { desc = "[F]ind [K]eymaps" })
+    m("n", "<leader>ff", tl.find_files, { desc = "[F]ind [F]iles" })
+    m("n", "<leader>fs", tl.builtin, { desc = "[F]ind [S]elect Telescope" })
+    m("n", "<leader>fw", tl.grep_string, { desc = "[F]ind current [W]ord" })
+    m("n", "<leader>fg", tl.live_grep, { desc = "[F]ind by [G]rep" })
+    m("n", "<leader>fd", tl.diagnostics, { desc = "[F]ind [D]iagnostics" })
+    m("n", "<leader>fr", tl.resume, { desc = "[F]ind [R]esume" })
+    m("n", "<leader>f.", tl.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+    m("n", "<leader><leader>", tl.buffers, { desc = "[ ] Find existing buffers" })
+
+    m("n", "<leader>f/", function()
+      tl.live_grep({
         grep_open_files = true,
         prompt_title = "Live Grep in Open Files",
       })
     end, { desc = "[F]ind [/] in Open Files" })
 
     -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set("n", "<leader>fn", function()
-      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    m("n", "<leader>fn", function()
+      tl.find_files({ cwd = vim.fn.stdpath("config") })
     end, { desc = "[F]ind [N]eovim files" })
   end,
 }
