6453308ea9b4e6ae81c1b9db498933c4be38be19
mimvoid <mimvoid@proton.me>
2024-12-29 23:37:17 -0500

    rework moonfall eve colors

--- lua/zinnia/colors.lua
+++ lua/zinnia/colors.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 M.hues = {
-  red = "#f281a6",
-  magenta = "#c37ac9",
-  yellow = "#efa4b7",
-  green = "#6990d6",
+  red = "#f280aa",
+  magenta = "#d87dd6",
+  yellow = "#eda2b5",
+  green = "#7098de",
   cyan = "#b1b5e4",
-  blue = "#9673de",
+  blue = "#a675eb",
 }
 
 M.base16 = {
   base00 = "#25223a",
-  base01 = "#312c48",
-  base02 = "#38344e",
-  base03 = "#5d5573",
-  base04 = "#9e92a5",
+  base01 = "#302c47",
+  base02 = "#37324f",
+  base03 = "#5c5478",
+  base04 = "#9c92aa",
   base05 = "#e8dfdd",
   base06 = "#e8dfdd",
-  base07 = "#44415a",
+  base07 = "#44415b",
 
   base08 = M.hues.red,
   base09 = M.hues.magenta,
@@ -27,7 +27,7 @@ M.base16 = {
   base0D = M.hues.blue,
   base0E = M.hues.magenta,
 
-  base0F = "#55486b",
+  base0F = "#52486d",
 }
 
 return M
