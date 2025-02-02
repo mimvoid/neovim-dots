13b7a7ce1e9b64aaa6f07491d264a741d7a1e747
mimvoid <mimvoid@proton.me>
2025-02-01 20:16:05 -0500

    disable autopair in commandline

--- lua/zinnia/plugins/autopair.lua
+++ lua/zinnia/plugins/autopair.lua
@@ -3,6 +3,6 @@ return {
   event = { "InsertEnter", "CmdlineEnter" },
   branch = "v0.6", --recommended as each new version will have breaking changes
   opts = {
-    --Config goes here
+    cmap = false,
   },
 }
