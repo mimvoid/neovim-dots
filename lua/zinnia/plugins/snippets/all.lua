bae627a34519b0a9c275a45f38106aa9eeb6bfbc
mimvoid <mimvoid@proton.me>
2025-03-09 15:01:18 -0400

    update iso date time snippet

--- lua/zinnia/plugins/snippets/all.lua
+++ lua/zinnia/plugins/snippets/all.lua
@@ -2,12 +2,15 @@ function snippets(ls)
   local s = ls.snippet
   local t = ls.text_node
 
-  function hugo_date()
-    return os.date("%Y-%m-%dT%H:%M:%S-05:00")
+  -- ISO date time stamp, with timezone as +00:00
+  function disotz()
+    local z = os.date("%z")
+    local utc_offset = string.sub(z, 1, 3) .. ":" .. string.sub(z, 4, 5)
+    return os.date("%Y-%m-%dT%H:%M:%S") .. utc_offset
   end
 
   return {
-    s("hugodate", { t(hugo_date()) })
+    s("disotz", { t(disotz()) })
   }
 end
 
