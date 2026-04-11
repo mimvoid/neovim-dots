97cf35cb6bb1c718b408d43d00d94551c6178b1c
mimvoid <mimvoid@proton.me>
2026-04-11 14:09:00 -0400

    Customize undotree window

--- lua/zinnia/core/init.lua
+++ lua/zinnia/core/init.lua
@@ -7,4 +7,10 @@ require("zinnia.core.lsps")
 -- Built-in plugins
 
 vim.cmd.packadd("nvim.undotree")
-vim.keymap.set("n", "<leader>u", require("undotree").open)
+vim.keymap.set("n", "<leader>u", function()
+  if not require("undotree").open() then
+    -- A new window was opened, disable line numbers
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end
+end)
